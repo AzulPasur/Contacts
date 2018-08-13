fc76a692f76511b42e23371491a72632584393df
AzulPasur <gtwhsgh@gmail.com>
2018-08-13 16:33:39 +0800

    加入bootstrap和jquery

--- src/com/hadon/guantong/controller/ContactController.java
+++ src/com/hadon/guantong/controller/ContactController.java
@@ -21,7 +21,7 @@ import com.hadon.guantong.service.ContactService;
 /*
 请求映射，链接地址第一层满足指定的value则进入这个控制类中的方法
  */
-@RequestMapping(value = "/contacts")
+//@RequestMapping(value = "/contacts")
 /*
 指定当前类为控制类，value可以重命名（无法自动装配）
 不使用注解时，需实现org.springframework.web.servlet.mvc.Controller接口
