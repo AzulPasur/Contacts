43069250c4dff5786e358ea49110fa2fe622ae3f
AzulPasur <gtwhsgh@gmail.com>
2018-08-16 10:42:18 +0800

    消除代码冗余

--- WebContent/pageSource/js/contacts.js
+++ WebContent/pageSource/js/contacts.js
@@ -11,29 +11,7 @@ $.ajax({
 	contentType : "application/json; charset=utf-8",
 	success :function(data){
 		var table = $("table");
-		table.append("<tbody>");
-		$.each(data,function(n,obj){
-			var tr;
-			if(n<10){
-				tr = "<tr>";
-			} else {
-				tr = "<tr hidden>"
-			}						 
-			$.each(obj,function(name,value){
-				var td;
-				if(name=="id"){
-					tr += "<td hidden>" + value + "</td>";return true;
-				} 
-				if(name=="sex") {
-					value=value?"男":"女";
-				}
-				td = "<td>" + value + "</td>";
-				tr += td;
-			});
-			tr += "</tr>";
-			table.append(tr);
-		});
-		table.append("</tbody>");
+		buildTable(data, table);
 	}
 });
 function findContacts(wd){
@@ -47,35 +25,12 @@ function findContacts(wd){
 		success :function(data){
 			var table = $("table");
 			console.log(JSON.stringify(data));
+			table.empty();
 			if(data.length==0){
-				table.empty();
 				table.append("<tr><td>无搜索结果</td></tr>");
-				return;
+			} else {
+				buildTable(data, table);
 			}
-			$("tbody").remove();
-			table.append("<tbody>");
-			$.each(data,function(n,obj){
-				var tr;
-				if(n<10){
-					tr = "<tr>";
-				} else {
-					tr = "<tr hidden>"
-				}						 
-				$.each(obj,function(name,value){
-					var td;
-					if(name=="id"){
-						tr += "<td hidden>" + value + "</td>";return true;
-					} 
-					if(name=="sex") {
-						value=value?"男":"女";
-					}
-					td = "<td>" + value + "</td>";
-					tr += td;
-				});
-				tr += "</tr>";
-				table.append(tr);
-			});
-			table.append("</tbody>");
 		},
 		error :function(XMLHttpRequest, textStatus){
 			console.log(textStatus);
@@ -84,4 +39,40 @@ function findContacts(wd){
 			console.log(textStatus);
 		}
 	});
+}
+
+function buildTable(data, table){
+	table.append("<thead class='thead-light'>" +
+		"<tr>" +
+			"<th>姓名</th>" +
+			"<th>性别</th>" +
+			"<th>部门</th>" +
+			"<th>职务</th>" +
+			"<th>手机</th>" +
+			"<th>邮箱</th>" +
+			"</tr>" +
+		"</thead>");
+	table.append("<tbody>");
+	$.each(data,function(n,obj){
+		var tr;
+		if(n<10){
+			tr = "<tr>";
+		} else {
+			tr = "<tr hidden>"
+		}						 
+		$.each(obj,function(name,value){
+			var td;
+			if(name=="id"){
+				tr += "<td hidden>" + value + "</td>";return true;
+			} 
+			if(name=="sex") {
+				value=value?"男":"女";
+			}
+			td = "<td>" + value + "</td>";
+			tr += td;
+		});
+		tr += "</tr>";
+		table.append(tr);
+	});
+	table.append("</tbody>");
 }
