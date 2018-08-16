47059a65d4f88d457b547ef37e89c55c6a7d252c
AzulPasur <gtwhsgh@gmail.com>
2018-08-16 17:06:46 +0800

    前端实现分页

--- WebContent/pageSource/js/contacts.js
+++ WebContent/pageSource/js/contacts.js
@@ -1,6 +1,8 @@
 /**
  * 
  */
+
+//页面初始化
 $("#wd").val("");
 $.ajax({
 	url :"/Contacts/show",
@@ -10,10 +12,12 @@ $.ajax({
 	data :{},
 	contentType : "application/json; charset=utf-8",
 	success :function(data){
-		var table = $("table");
-		buildTable(data, table);
+		var tableList = $("#tableList");
+		buildTable(data, tableList);
 	}
 });
+
+//查找
 function findContacts(wd){
 	wd = wd.value;
 	$.ajax({
@@ -23,13 +27,14 @@ function findContacts(wd){
 		datatype :"json",
 		data :{keyWord : wd},
 		success :function(data){
-			var table = $("table");
+			var tableList = $("#tableList");
 			console.log(JSON.stringify(data));
-			table.empty();
+			tableList.empty();
+			$(".pagination").empty();
 			if(data.length==0){
-				table.append("<tr><td>无搜索结果</td></tr>");
+				tableList.append("<p>无搜索结果</p>");
 			} else {
-				buildTable(data, table);
+				buildTable(data, tableList);
 			}
 		},
 		error :function(XMLHttpRequest, textStatus){
@@ -41,29 +46,36 @@ function findContacts(wd){
 	});
 }
 
-function buildTable(data, table){
-	table.append("<thead class='thead-light'>" +
-		"<tr>" +
-			"<th>姓名</th>" +
-			"<th>性别</th>" +
-			"<th>部门</th>" +
-			"<th>职务</th>" +
-			"<th>手机</th>" +
-			"<th>邮箱</th>" +
-			"</tr>" +
-		"</thead>");
-	table.append("<tbody>");
+//构建表格
+function buildTable(data, tableList){
+	var table;
+	activePage = "1";
+	pageNum = Math.floor((data.length - 1) / 10 + 1);
 	$.each(data,function(n,obj){
-		var tr;
-		if(n<10){
-			tr = "<tr>";
-		} else {
-			tr = "<tr hidden>"
-		}						 
+		//当n为10的倍数时,新建一张表
+		if(!(n%10)){			
+			table = $("<table class='table table-hover'></table>");
+			table.attr("id", "table"+(n/10+1));
+			table.append("<thead class='thead-light'>" +
+				"<tr>" +
+					"<th>姓名</th>" +
+					"<th>性别</th>" +
+					"<th>部门</th>" +
+					"<th>职务</th>" +
+					"<th>手机</th>" +
+					"<th>邮箱</th>" +
+					"</tr>" +
+				"</thead>");
+			table.append("<tbody>");
+			if(table.attr("id")!="table1"){
+				table.hide();
+			}
+		}
+		var tr = "<tr>";					 
 		$.each(obj,function(name,value){
 			var td;
 			if(name=="id"){
-				tr += "<td hidden>" + value + "</td>";return true;
+				tr += "<td hidden>" + value + "</td>";return true;//联系人的id被隐藏
 			} 
 			if(name=="sex") {
 				value=value?"男":"女";
@@ -73,6 +85,13 @@ function buildTable(data, table){
 		});
 		tr += "</tr>";
 		table.append(tr);
+		//表中每满10条记录或数据读完时，将表加入tableList
+		if(n%10==9 || n==data.length-1){
+			table.append("</tbody>");
+			tableList.append(table);
+		}
 	});
-	table.append("</tbody>");
+	if(pageNum){
+		initPaging();
+	}
 }
---
+++ WebContent/pageSource/js/paging.js
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+
+var activePage = $(".active").attr("id");
+var pageNum;
+
+//下一页
+function nextPage(){
+	if(hasNext()) goToPage(activePage*1+1);
+}
+
+//末页
+function lastPage(){
+	goToPage(pageNum);
+}
+
+//上一页
+function prevPage(){
+	if(hasPrev()) goToPage(activePage*1-1);
+}
+
+//首页
+function homePage(){
+	goToPage(1);
+}
+
+function hasNext(){
+	return (activePage*1) < pageNum;
+}
+
+function hasPrev(){
+	return (activePage*1) > 1;
+}
+//跳转指定页
+function goToPage(n){
+	$(".active").attr("class", "page-item");
+	$("#"+n).attr("class", "page-item active");
+	tableList = $("#tableList");
+	$("table").each(function(){
+		table = $(this);//获取当前元素
+		if(table.attr("id")==("table"+n)){
+			table.show();
+		} else {
+			table.hide();
+		}
+	});
+	activePage = n;
+}
+
+//初始化
+function initPaging(){
+	ul = $(".pagination");
+	ul.append('<li class="page-item"><a class="page-link" href="#" onclick="homePage()">首页</a></li>');
+	ul.append('<li class="page-item"><a class="page-link" href="#" onclick="prevPage()">上一页</a></li>');
+	for(var i=0; i<pageNum; i++){
+		currentNum=i+1;
+		var active = "active";
+		if(!i){
+			active="active";
+		} else {
+			active="";
+		}
+		ul.append('<li class="page-item '+active+'" id="'+currentNum+'">' +
+			'<a class="page-link" href="#" onclick="goToPage('+currentNum+')">'+currentNum+'</a>' +
+		'</li>');
+	}
+	ul.append('<li class="page-item"><a class="page-link" href="#" onclick="nextPage()">下一页</a></li>');
+	ul.append('<li class="page-item"><a class="page-link" href="#" onclick="lastPage()">尾页</a></li>');
+}
